5bc90ea93862da266d0378751f5222acf89ea04d
Ravi <dahalravindra@gmail.com>
2021-02-27 19:33:15 +0545

    Dispatched actions and updated the global state accordingly :sparkles:

--- src/components/SongsList.js
+++ src/components/SongsList.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { selectSong } from "../actions";
+import SongDetail from "./SongDetail";
 
 class SongList extends Component {
   renderList() {
@@ -7,7 +9,12 @@ class SongList extends Component {
       return (
         <div className="item" key={song.title}>
           <div className="right floated content">
-            <button className="ui button primary">Select</button>
+            <button
+              onClick={() => this.props.selectSong(song)}
+              className="ui button primary"
+            >
+              Select
+            </button>
           </div>
           <div className="content">{song.title}</div>
         </div>
@@ -16,7 +23,12 @@ class SongList extends Component {
   }
   render() {
     console.log(this.props);
-    return <div className="ui divided list">{this.renderList()}</div>;
+    return (
+      <>
+        <div className="ui divided list">{this.renderList()}</div>
+        <SongDetail />{" "}
+      </>
+    );
   }
 }
 
@@ -24,4 +36,4 @@ const mapStateToProps = (state) => {
   return { songs: state.songs };
 };
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, { selectSong })(SongList);
